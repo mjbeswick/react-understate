637f9d8ae5956f72f3debf84b3a9925f6b95e393
Michael Beswick <michael.beswick@tesco.com>
2025-09-01 10:26:04 +0100

    cleanup: remove traffic-light-xstate example
    - Remove outdated traffic light example that was using XState
    - Clean up example directory to focus on current demos
    - Keep calculator, todo-app, and deep-freezing-demo examples

--- examples/traffic-light-xstate/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import TrafficLightWidget from './TrafficLightWidget.tsx';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <TrafficLightWidget />
-  </React.StrictMode>
-);
